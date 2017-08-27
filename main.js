23b98d14c2ec01ef986f50c238f4d07222486905
guanyuxin <guanyuxin128@gmail.com>
2017-08-27 11:37:40 +0800

    v2

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ const electron = require('electron')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const {BrowserWindow, systemPreferences} = electron;
 
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
+var https = require('https');
+var packageConfig = require('./package.json');
+
+let browserOptions = {width: 800, height: 400, frame: false}
+
+// Make the window transparent only if the platform supports it.
+if (process.platform !== 'win32' || systemPreferences.isAeroGlassEnabled()) {
+  browserOptions.transparent = true
+}
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -17,7 +26,7 @@ let initWindow
 
 function createInitWindow () {
 
-  initWindow = new BrowserWindow({width: 800, height: 400, frame: false,  transparent: true})
+  initWindow = new BrowserWindow(browserOptions)
 
   // and load the index.html of the app.
   initWindow.loadURL(url.format({
@@ -77,7 +86,59 @@ app.on('activate', function () {
   }
 })
 
-fs.writeFileSync('index2.html', '123');
+function checkUpdate() {
+  getHttpsData('https://raw.githubusercontent.com/guanyuxin/snow/master/package.json', function (res) {
+    var data = JSON.parse(res);
+    if (packageConfig.buildVer < data.buildVer) {
+      console.log('need update');
+      for (var i = 0; i < data.files.length; i++) {
+        update(data.files[i]);
+      }
+    }
+  })
+}
+checkUpdate();
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+function update(file) {
+  getHttpsData('https://raw.githubusercontent.com/guanyuxin/snow/master/' + file, function (res) {
+      console.log(file + ' updated');
+    fs.writeFileSync(file, res);
+  })
+}
+
+
+
+function getHttpsData(filepath, success, error) {
+  // 回调缺省时候的处理
+  success = success || function () {};
+  error = error || function () {};
+
+  var url = 'https://raw.githubusercontent.com/username/project-name/master/' + filepath + '?r=' + Math.random();
+
+  https.get(url, function (res) {
+    var statusCode = res.statusCode;
+
+    if (statusCode !== 200) {
+        // 出错回调
+        error();
+        // 消耗响应数据以释放内存
+        res.resume();
+        return;
+    }
+
+    res.setEncoding('utf8');
+    var rawData = '';
+    res.on('data', function (chunk) {
+      rawData += chunk;
+    });
+
+    // 请求结束
+    res.on('end', function () {
+      // 成功回调
+      success(rawData);
+    }).on('error', function (e) {
+      // 出错回调
+      error();
+    });
+  });
+};
