60fc7b924c8ead3e94c1b633e31eb9d1dab85d61
guanyuxin <guanyuxin128@gmail.com>
2017-09-03 17:08:19 +0800

    d

--- main.js
+++ main.js
@@ -97,43 +97,47 @@ var https = require('https');
 var fs = require('fs');
 var packageConfig = require('./package.json')
 function checkUpdate(cb) {
-  process.noAsar = true;
   getHttpsData('https://raw.githubusercontent.com/guanyuxin/snow/master/package.json', function (res) {
     var data = JSON.parse(res);
     if (packageConfig.buildVer < data.buildVer) {
       sendToWin('updateInfo', '检测到更新');
       cb && cb(data);
+    } else {
+      sendToWin('updateInfo', '最新版本');
     }
   })
 }
 
 checkUpdate(function (data) {
-   var updateing = data.files.map(function(file, i) {
-    return new Promise(function (resolve, reject) {
-      getHttpsData('https://raw.githubusercontent.com/guanyuxin/snow/master/' + file, function (res) {
-        console.log('downloaded' + file);
-        fs.writeFile('./resources/app/updateTmp/' + file, res, function () {
-          sendToWin('updateInfo', '下载' + file);
-          resolve(file);
+  fs.mkdir('./resources/app/updateTmp', function () {
+    var updateing = data.files.map(function(file, i) {
+      return new Promise(function (resolve, reject) {
+        getHttpsData('https://raw.githubusercontent.com/guanyuxin/snow/master/' + file, function (res) {
+          console.log('downloaded' + file);
+          fs.writeFile('./resources/app/updateTmp/' + file, res, function () {
+            sendToWin('updateInfo', '下载' + file);
+            resolve(file);
+          });
+        }, () => {
+          reject();
         });
-      }, () => {
-        reject();
       });
     });
-  })
-  Promise.all(updateing).then((res) => {
-    sendToWin('updateInfo', '下载完成');
-    var moveing = res.map((file, i) => {
-      return new Promise((resolve, reject) =>{
-        fs.rename('./resources/app/updateTmp/' + file, './resources/app/' + file, () => {
-          resolve(file);
+    Promise.all(updateing).then((res) => {
+      sendToWin('updateInfo', '下载完成');
+      var moveing = res.map((file, i) => {
+        return new Promise((resolve, reject) =>{
+          fs.rename('./resources/app/updateTmp/' + file, './resources/app/' + file, () => {
+            resolve(file);
+          })
         })
       })
+      return Promise.all(moveing)
+    }, () => {
+      sendToWin('updateInfo', '更新失败');
+    }).then(()=>{
+      sendToWin('updateInfo', '更新完毕，重启生效');
     })
-  }, () => {
-    sendToWin('updateInfo', '更新失败');
-  }).then(()=>{
-    sendToWin('updateInfo', '更新完毕，重启生效');
   })
 });
 
@@ -149,11 +153,11 @@ function getHttpsData(filepath, success, error) {
     var statusCode = res.statusCode;
 
     if (statusCode !== 200) {
-        // 出错回调
-        error();
-        // 消耗响应数据以释放内存
-        res.resume();
-        return;
+      // 出错回调
+      error();
+      // 消耗响应数据以释放内存
+      res.resume();
+      return;
     }
 
     res.setEncoding('utf8');
